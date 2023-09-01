ea6b38da2e69287617c1b8773097ed5071e2308c
TomGrzembke <tomgrzembke04@gmail.com>
2023-09-01 14:20:46 +0200

    Added Slider and toggle function

--- Assets/+++workdata/scripts/MainMenu/MainMenuManager.cs
+++ Assets/+++workdata/scripts/MainMenu/MainMenuManager.cs
@@ -1,20 +1,41 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 namespace JustASpoonful
 {
     public class MainMenuManager : MonoBehaviour
     {
         #region Cashes
+        [Header("Menu")]
         [SerializeField] GameObject[] menuObjects;
         [SerializeField] GameObject menuBG;
         [SerializeField] Animator transitionAnim;
+        [Header("Settings")]
+        [SerializeField] Slider musicSlider;
+        [SerializeField] Slider sfxSlider;
+        [SerializeField] AudioMixer audioMixer;
+        [SerializeField] Toggle screenToggle;
         #endregion
 
         #region Variables
 
         #endregion
+
+        void Start() => CheckSavedSettings();
+
+        void CheckSavedSettings()
+        {
+            SetMusicVolume(PlayerPrefs.GetFloat("musicVolume"));
+            SetSfxVolume(PlayerPrefs.GetFloat("sfxVolume"));
+
+            //Gets and sets the State of the screentoggle
+            bool screenToggleState = PlayerPrefs.GetInt("fullscreenState") == 0;
+            Screen.fullScreen = screenToggleState;
+            screenToggle.isOn = screenToggleState;
+        }
 
         /// <summary>
         /// Enables one given object and disables the rest
@@ -39,6 +60,42 @@ namespace JustASpoonful
             {
                 menuObjects[i].SetActive(false);
             }
+        }
+
+        public void OnMusicSliderChanged()
+        {
+            float volume = musicSlider.value;
+            audioMixer.SetFloat("musicVolume", volume);
+            PlayerPrefs.SetFloat("musicVolume", volume);
+        }
+
+        public void SetMusicVolume(float volume)
+        {
+            musicSlider.value = volume;
+        }
+
+        public void OnSfxSliderChanged()
+        {
+            float volume = sfxSlider.value;
+            audioMixer.SetFloat("sfxVolume", volume);
+            PlayerPrefs.SetFloat("sfxVolume", volume);
+        }
+
+        public void SetSfxVolume(float volume)
+        {
+            sfxSlider.value = volume;
+        }
+
+        public void FullScreenToggle(bool state)
+        {
+            state = screenToggle.isOn;
+            Screen.fullScreen = state;
+            PlayerPrefs.SetInt("fullscreenState", state ? 0 : 1);
+        }
+
+        public void OpenURL(string link)
+        {
+            Application.OpenURL(link);
         }
 
         #region Loadscene overload
