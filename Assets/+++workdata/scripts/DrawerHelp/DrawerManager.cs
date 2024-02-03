0b84d58595c645e7b0b2dc9055f93d19ef35e4f6
TomGrzembke <tomgrzembke04@gmail.com>
2024-02-04 00:31:27 +0100

    Working drawer dragging and Puzzle pickup

--- Assets/+++workdata/scripts/DrawerHelp/DrawerManager.cs
+++ Assets/+++workdata/scripts/DrawerHelp/DrawerManager.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 namespace JustASpoonful
@@ -10,17 +11,51 @@ namespace JustASpoonful
         [SerializeField] Collider2D drawerCollider;
         [SerializeField] Transform drawerTrans;
         [SerializeField] GameObject drawerHitbox;
+        [SerializeField] float dragWidth = 5;
+        [SerializeField] float moveTime;
+
+        Vector3 lastDragPos;
+        Vector3 initialPos;
+        Vector3 rightPosition;
+        Coroutine dragRoutine;
+
+        void Start()
+        {
+            initialPos = drawerTrans.position;
+            rightPosition = initialPos.ChangeAxis(Axis.X, initialPos.x + dragWidth);
+        }
 
         public void OnClick()
         {
             drawerSprite.sprite = drawerHitbox.activeSelf ? drawerClosed : drawerOpen;
+
             drawerHitbox.SetActive(!drawerHitbox.activeSelf);
             drawerCollider.enabled = !drawerHitbox.activeSelf;
 
+            lastDragPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         }
         public void OnDrag()
         {
-            
+            Vector3 newDragPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            bool toRight = lastDragPos.x - newDragPos.x < 0;
+
+            if (dragRoutine != null)
+                StopCoroutine(dragRoutine);
+            dragRoutine = StartCoroutine(LerpToTarget(toRight));
+        }
+
+        IEnumerator LerpToTarget(bool toRight)
+        {
+            float timeSpent = 0;
+            Vector3 drawerPos = drawerTrans.position;
+
+            while (timeSpent < moveTime)
+            {
+                timeSpent += Time.deltaTime;
+                drawerTrans.position = Vector3.Lerp(drawerPos, toRight ? rightPosition : initialPos, timeSpent / moveTime);
+                yield return null;
+            }
         }
     }
 }
