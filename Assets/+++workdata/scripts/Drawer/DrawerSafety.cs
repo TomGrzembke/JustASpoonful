1f40fae387e4e641ae9d5e9ad0fa874a507b02a7
TomGrzembke <tomgrzembke04@gmail.com>
2024-02-06 21:11:42 +0100

    Working flashlight, drawer safety rework

--- Assets/+++workdata/scripts/Drawer/DrawerSafety.cs
+++ Assets/+++workdata/scripts/Drawer/DrawerSafety.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace JustASpoonful
@@ -7,31 +8,50 @@ namespace JustASpoonful
     {
         [SerializeField] Transform returnPoint;
         [SerializeField] float returnSpeed = 25;
-        GameObject returnObj;
+
+        [SerializeField] Dictionary<GameObject, Coroutine> returnList = new();
+
         void OnTriggerEnter2D(Collider2D col)
         {
             if (col.CompareTag("Movable"))
             {
-                returnObj = col.gameObject;
-                StartCoroutine(ReturnToPos());
+                if (!returnList.ContainsKey(col.gameObject))
+                {
+                    Coroutine dragRoutine = StartCoroutine(ReturnToPos(col.gameObject));
+                    returnList.Add(col.gameObject, dragRoutine);
+                }
             }
         }
 
-        IEnumerator ReturnToPos()
+        void OnTriggerExit2D(Collider2D col)
+        {
+            if (col.CompareTag("Movable"))
+            {
+                if (returnList.TryGetValue(col.gameObject, out Coroutine outRountine))
+                    StopCoroutine(outRountine);
+
+                returnList.Remove(col.gameObject);
+            }
+        }
+
+        IEnumerator ReturnToPos(GameObject returnObj)
         {
             Movable currentMovable = returnObj.GetComponent<Movable>();
             float lerpProgress = 0;
             yield return new WaitUntil(() => !currentMovable.isBeingDragged);
 
+            Vector3 currentPos = returnObj.transform.position;
             //Move Obj back logic
             while (returnObj.transform.position.magnitude != returnPoint.position.magnitude)
             {
                 if (currentMovable.isBeingDragged)
                     yield break;
                 lerpProgress += Time.deltaTime / returnSpeed;
-                returnObj.transform.position = Vector3.Lerp(returnObj.transform.position, returnPoint.position, lerpProgress);
+                returnObj.transform.position = Vector3.Lerp(currentPos, returnPoint.position, lerpProgress);
                 yield return null;
             }
+
+            returnList.Remove(returnObj);
         }
     }
 }
