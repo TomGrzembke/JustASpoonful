0065806e9604f25b9ff0785770e50b67dcade04a
TomGrzembke <tomgrzembke04@gmail.com>
2024-02-07 16:21:15 +0100

    Mobile naviagation working

--- Assets/+++workdata/scripts/Mobile/MobileManager.cs
+++ Assets/+++workdata/scripts/Mobile/MobileManager.cs
@@ -1,21 +1,71 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Device;
 
 namespace JustASpoonful
 {
     public class MobileManager : MonoBehaviour
     {
-        // Start is called before the first frame update
+        [SerializeField] List<GameObject> screenHistory;
+        [SerializeField] GameObject mobile;
+        [SerializeField] GameObject homeScreen;
+
+        [SerializeField] GameObject currentScreen;
+
         void Start()
         {
-        
+            currentScreen = homeScreen;
         }
 
-        // Update is called once per frame
-        void Update()
+        public void MobileSetActive(bool condition)
         {
-        
+            mobile.SetActive(condition);
+        }
+
+        public void MobileToggle()
+        {
+            mobile.SetActive(!mobile.activeInHierarchy);
+        }
+
+        public void SwitchScreen(GameObject screen)
+        {
+            screenHistory.Add(currentScreen);
+            currentScreen = screen;
+
+            DeactivateHistoryExcept(screen);
+
+            currentScreen.SetActive(true);
+        }
+
+         void DeactivateHistoryExcept(GameObject screen)
+        {
+            for (int i = 0; i < screenHistory.Count; i++)
+            {
+                screenHistory[i].SetActive(screenHistory[i] == screen);
+            }
+        }
+
+        public void BackScreen()
+        {
+            if (screenHistory.Count < 1) return;
+
+            currentScreen.SetActive(false);
+
+            GameObject lastScreen = screenHistory[screenHistory.Count - 1];
+            DeactivateHistoryExcept(lastScreen);
+            currentScreen = lastScreen;
+
+            screenHistory.RemoveAt(screenHistory.Count - 1);
+        }
+
+        public void HomeScreen()
+        {
+            currentScreen.SetActive(false);
+            screenHistory.Add(currentScreen);
+
+            currentScreen = homeScreen;
+            DeactivateHistoryExcept(homeScreen);
+            homeScreen.SetActive(true);
         }
     }
 }
