efb2344a7b46018486fd36f159d7d1a64e464351
TomGrzembke <tomgrzembke04@gmail.com>
2024-02-09 19:39:49 +0100

    EndCutscene function na dlayout to canvas

--- Assets/+++workdata/scripts/StartCutscene/EndscreenManager.cs
+++ Assets/+++workdata/scripts/StartCutscene/EndscreenManager.cs
@@ -3,41 +3,59 @@ using UnityEngine;
 
 public class EndscreenManager : MonoBehaviour
 {
-    [SerializeField] GameObject[] imagesFirstPart;
-    [SerializeField] GameObject[] imagesSecondPart;
+    [SerializeField] CanvasGroup[] imagesFirstPart;
+    [SerializeField] CanvasGroup[] imagesSecondPart;
 
     [SerializeField] float defaultInterval = 4;
 
     IEnumerator Start()
     {
-        yield return new WaitForSeconds(defaultInterval);
+        yield return new WaitForSeconds(2);
 
         for (int i = 0; i < imagesFirstPart.Length; i++)
         {
-            imagesFirstPart[i].SetActive(true);
+            StartCoroutine(FadeInGroup(imagesFirstPart[i]));
 
             yield return new WaitForSeconds(defaultInterval);
         }
 
         for (int i = 0; i < imagesFirstPart.Length; i++)
         {
-            imagesFirstPart[i].SetActive(false);
+            StartCoroutine(FadeOutGroup(imagesFirstPart[i]));
         }
+
+        yield return new WaitForSeconds(defaultInterval);
 
         for (int i = 0; i < imagesSecondPart.Length; i++)
         {
-            imagesSecondPart[i].SetActive(true);
+            StartCoroutine(FadeInGroup(imagesSecondPart[i]));
 
             yield return new WaitForSeconds(defaultInterval);
         }
-
-        ToMain();
     }
 
-    public void SkipCutscene()
+    IEnumerator FadeInGroup(CanvasGroup canvasGroup)
     {
-        StopAllCoroutines();
-        ToMain();
+        float fadeTime = 0;
+
+        while (fadeTime < defaultInterval)
+        {
+            fadeTime += Time.deltaTime;
+            canvasGroup.alpha = fadeTime / defaultInterval;
+            yield return null;
+        }
+    }
+
+    IEnumerator FadeOutGroup(CanvasGroup canvasGroup)
+    {
+        float fadeTime = 0;
+
+        while (fadeTime < defaultInterval)
+        {
+            fadeTime += Time.deltaTime;
+            canvasGroup.alpha = -(-1 + Mathf.Clamp01(fadeTime / defaultInterval));
+            yield return null;
+        }
     }
 
     public void ToMain()
